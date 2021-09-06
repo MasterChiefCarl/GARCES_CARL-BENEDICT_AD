6f9a6e53fa58177b3e3e2fa4690b0f2eab4f649f
Carl Benedict Garces <carlgarces7@gmail.com>
2021-09-06 13:03:38 +0800

    Update 2

--- 2_JS/HelloWorld.js
+++ 2_JS/HelloWorld.js
@@ -1,1 +1,3 @@
+console.log("Hello World!");
+//just in case i might not be following instructions. redact comments on next lines for trolling purposes only
 //console.log("Hello World my dudes\nIts Me Your Looking For\nI've Been Seeing in your eyes");
--- 2_JS/VariableType.js
+++ 2_JS/VariableType.js
@@ -1,14 +1,31 @@
 var number = 1;
-var a = [1, 2, 3, 4, 5, 6];
+var a = "Welcome to my assignment that needs no futher introduction tbh. Yeah IDK why im doing this\nSince you kept reading ill just rickroll u in the process.\nhere's a link: https://www.youtube.com/watch?v=o-YBDTqX_ZU";
 var b = { one: 1, "two": "two", three: [3] };
 // symbolic index : value
-var c;
+var c = "Honkai Impact";
 var d = null;
+
+b.four = "Hi y'all!";
+b["five"] = ["Genshin", "Impact"];
+b.six = false;
+
+console.log("Variable Types Assignment\nBy Carl Garces\n\n");
+
+console.log(`The variable ${number} is of the type ${typeof number} `);
+console.log("\n\n");
+
+console.log(typeof a);
+console.log(a);
+console.log("\n\n");
 
 console.log(typeof c);
 console.log(`The variable ${c} is of the type ${typeof c} `);
+console.log("\n\n");
+
+console.log(typeof d);
+console.log(d);
+console.log("\n\n");
+
+console.log(typeof b);
 console.log(b);
-b.four = "Hi y'all!";
-b["five"] = ["Genshin", "Impact"];
-b.six;
-console.log(b);
+console.log("\n\n");
