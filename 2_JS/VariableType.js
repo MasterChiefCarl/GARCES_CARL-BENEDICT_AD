1beaff6cb51e9c411a99e9c20310406fe423f381
Carl Benedict Garces <carlgarces7@gmail.com>
2021-09-06 11:41:40 +0800

    VariableType.js Fix
    needed to add some files forgot to save hehehe

--- 2_JS/VariableType.js
+++ 2_JS/VariableType.js
@@ -1,8 +1,14 @@
 var number = 1;
 var a = [1, 2, 3, 4, 5, 6];
 var b = { one: 1, "two": "two", three: [3] };
+// symbolic index : value
 var c;
 var d = null;
 
 console.log(typeof c);
 console.log(`The variable ${c} is of the type ${typeof c} `);
+console.log(b);
+b.four = "Hi y'all!";
+b["five"] = ["Genshin", "Impact"];
+b.six;
+console.log(b);
