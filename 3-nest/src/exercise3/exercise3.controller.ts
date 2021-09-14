6efd962d39881f8e2d8631e0c7fb453f4705eb52
Carl Benedict Garces <carlgarces7@gmail.com>
2021-09-14 17:11:50 +0800

    Exercise 4 (user) Starting Point
    This is a copy of the apps-dev resource and serves as a starting guide for me as I begin journeying though this new chapter of TORTURE. not you sir of course :D. I hope you read this heheheh 
    
    Changes:
    Changes in exercise3 (refer to recording on what to do).

--- 3-nest/src/exercise3/exercise3.controller.ts
+++ 3-nest/src/exercise3/exercise3.controller.ts
@@ -1,26 +1,44 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Exercise3Service } from './exercise3.service';
 
 @Controller('exercise3')
 export class Exercise3Controller {
-    constructor (private readonly e3: Exercise3Service){}
+  constructor(private readonly e3: Exercise3Service) {}
 
-        @Get('/loopsTriangle/:height')
-        loopsTriangle(@Param('height')height: string){
-            var parsedHeight:number= parseInt(height);
-            return this.e3.loopsTriangle(parsedHeight);
-        }
+  @Get('/loopsTriangle/:height')
+  loopsTriangle(@Param('height') height: string) {
+    var parsedHeight: number = parseInt(height);
+    return this.e3.loopsTriangle(parsedHeight);
+  }
 
-        @Get('/hello/:name')
-        helloWorld(@Param('name')name: string){
-            var nameString:string = name;
-            return this.e3.helloWorld(nameString);
-        }
+  @Get('/car/:id')
+  getOne(@Param("id") id:string) {
+    return this.e3.getCar(id);
+  }
+  @Post('/addCar')
+  addCar(@Body() body: any) {
+    return this.e3.addCar(body);
+  }
 
-        @Get('/prime/:porc')
-        primeNumbers(@Param('porc')porc:string){ //porc - primeorcomposite
-            var parsedPorc:number = parseInt(porc);
-            return this.e3.primeNumbers(parsedPorc);
-        }
+  @Put('/replaceCar/:id')
+  replaceCar(@Param("id") id:string, @Body() body: any) {
+    return this.e3.replaceCar(id,body);
+  }
+
+  @Delete('/replaceCar/:id')
+  removeCar(@Param("id") id:string) {
+    return this.e3.deleteCar(id);
+  }
+
+  @Get('/addJoshCar2')
+  test2() {
+    return this.e3.addJoshCar2();
+    // return;
+  }
+
+  @Get('/logCars')
+  logCars() {
+    return this.e3.logAllCars();
+    // return;
+  }
 }
-
--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -1,51 +1,93 @@
+import { Car } from './car.model';
+import { HTML } from './html.helper';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class Exercise3Service {
-    
-    loopsTriangle(limit:number){
-        for (var col = 0; col < limit; col++) {
-            var triangleInput = "*"
-            for (var row = 0; row < col; ++row) {
-                triangleInput += "*";
-            }
-            console.log(triangleInput);
-        }
-    }
+   
+  private cars: Map<string,Car> = new Map<string,Car>();
 
-    helloWorld(name:string){
-        return "Hello, " + name + ", its me you're looking for. Charr!"
-    }
+  private cars2:Array<{}> = [{
+    "id":"CARL143",
+    "model":"Ferrari",
+    "color":"Black",
+    "wheels": {
+      "name": "Pirelli",
+      "radius":16
+    }	
+  },{
+    "id":"JOSH420",
+    "model":"Montero",
+    "color":"Red",
+    "wheels": {
+      "name": "Goodyear",
+      "radius":18
+    }	
+  }
+];
 
-    primeNumbers(select:number){
-        var looper;
-        var primeComposite = 0;
+searchCar(){
+  for(const car of this.cars2){
+    if(car['id']==="JOSH420"){
+      return car;
+    }
+  }
+}
+  getCar(id:string){
+    return this.cars.get(id).toJson();
+  }
 
 
+  loopsTriangle(height: number) {
+    var html: HTML = new HTML(); 
+    for (var i = 1; i <= height; i++) {
+      
+      var string = ''; 
+      var j = i;
+      while (j) {
+        string += '*';
+        j--;
+      }
+      html.add(html.div([string]));
+      console.log(string);
+    }
+    return html.renderScreenHTML();
+  }
 
-    for (looper = 2; looper <= select / 2; looper++) {
-        if (select % looper == 0) {
-            primeComposite = primeComposite + 1;
-        break;
-    }
-    }
-    if (select == 1 || select == 0) {
-        console.log(select + " is neither prime nor composite");
-        return select + " is neither prime nor composite"
-    } 
-    else 
-    {
+  addCar(car:any){
+     var newCar: Car; 
+     newCar = new Car(car?.model, car?.color, {name: car?.wheels.name, radius:car.wheels.radius});
+     this.cars.set(car.id, newCar);
+     this.logAllCars();
+  }
 
-        if (primeComposite == 0) {
-            
-            console.log(select + " is a Prime");
-            return select + " is a Prime"
-        } 
-        else {
-            console.log(select + " is a Composite");
-            return select + " is a Composite"
-        }
-    }
-    }
+  deleteCar(id:string){
+    if(this.cars.has(id))
+    this.cars.delete(id);
+    else console.log(id+" does not exist in database!");
+  }
+
+  replaceCar(id:string, car:any){
+    var newCar: Car; 
+    newCar = new Car(car?.model, car?.color, {name: car?.wheels.name, radius:car.wheels.radius});
+    this.cars.set(id, newCar);
+    this.logAllCars();
+  }
+
+  addJoshCar2(){
+    var joshuaCar: Car; 
+    joshuaCar = new Car("Montero", "Blue", {name: "Goodyear", radius:18});
+    this.cars.set("joshua", joshuaCar);
+    this.logAllCars();
+ }
+
+  logAllCars(){
+   for(const [key,car] of this.cars.entries()){
+     console.log(key);
+     car.log();
+   }
+  }
+
+
 
 }
