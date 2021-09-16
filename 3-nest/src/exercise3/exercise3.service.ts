380d087399c91a625f777343d42129703be5c6a2
Carl Benedict Garces <carlgarces7@gmail.com>
2021-09-17 07:23:11 +0800

    Removing some stuff that merged idk how this happened
    but at least code cleared it for me.... somehow

--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -4,53 +4,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class Exercise3Service {
-<<<<<<< HEAD
-    
-    loopsTriangle(limit:number){
-        for (var col = 0; col < limit; col++) {
-            var triangleInput = "*"
-            for (var row = 0; row < col; ++row) {
-                triangleInput += "*";
-            }
-            console.log(triangleInput);
-        }
-    }
-
-    helloWorld(name:string){
-        return "Hello, " + name + ", its me you're looking for. Charr!"
-    }
-
-    primeNumbers(select:number){
-        var looper;
-        var primeComposite = 0;
-
-
-
-    for (looper = 2; looper <= select / 2; looper++) {
-        if (select % looper == 0) {
-            primeComposite = primeComposite + 1;
-        break;
-    }
-    }
-    if (select == 1 || select == 0) {
-        console.log(select + " is neither prime nor composite");
-        return select + " is neither prime nor composite"
-    } 
-    else 
-    {
-
-        if (primeComposite == 0) {
-            
-            console.log(select + " is a Prime");
-            return select + " is a Prime"
-        } 
-        else {
-            console.log(select + " is a Composite");
-            return select + " is a Composite"
-        }
-    }
-    }
-=======
    
   private cars: Map<string,Car> = new Map<string,Car>();
 
@@ -136,6 +89,5 @@ searchCar(){
   }
 
 
->>>>>>> main
 
 }
