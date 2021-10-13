4785ad6b22859e13a239bfebb26597c18468a430
Carl Benedict Garces <carlgarces7@gmail.com>
2021-10-13 11:19:52 +0800

    Ehe Denedayo!

---
+++ 4-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+ -oimport { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portal';
+}
