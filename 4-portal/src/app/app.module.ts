8f6aa1afc5d8ccc625d0f379fac8e144c22b496d
Carl Benedict Garces <carlgarces7@gmail.com>
2021-10-18 11:27:32 +0800

    🔥🔥

--- 4-portal/src/app/app.module.ts
+++ 4-portal/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginModule } from './screens/login/login.module'
+import { LoginComponent } from './screens/login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './screens/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoginModule
-
-
-
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 4-portal/src/app/screens/login/login.component.ts
+++ 4-portal/src/app/screens/login/login.component.ts
@@ -1,40 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-
-
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router, private api: Axios) {}
 
-  constructor (private router:Router){};
+  ngOnInit(): void {}
 
+  fcEmail = new FormControl();
+  fcPassword = new FormControl();
 
-  title = 'portal';
-  private userCredential1:string = 'carlgarces7@gmail.com';
-  private userCredential2:string = 'trollwarlord69';
-
-  ngOnInit():void{}
-
-  users: Array<any> =[
-    {username: 'potato', password: 'tomato'},
-    {username: 'potato1', password: 'tomato1'},
-    {username: 'potato2', password: 'tomato2'},
-    {username: 'potato3', password: 'tomato3'},
-  ];
-
-  login(email:string, password:string){
-    if (email == this.userCredential1 && password == this.userCredential2){
-      this.router.navigate(["home"]);
-
-    }else {
-      alert ("incorrect credentials");
-      if (email !=this.userCredential1) console.log ("Wrong username");
-      if (password !=this.userCredential1) console.log ("Wrong password");
-
+  login() {
+    if (
+      this.fcEmail.value == 'daniel@gmail.com' &&
+      this.fcPassword.value == '12345678'
+    ) {
+      this.nav('home');
+    } else {
+      alert('Incorrect credentials');
+      console.log('Nagkakamali ka ng susi');
     }
   }
-
+  nav(destination: string) {
+    this.router.navigate([destination]);
+  }
 }
